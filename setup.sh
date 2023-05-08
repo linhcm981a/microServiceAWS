c532583ba8d2932c1c8dac507eda871a81b439fb
linhcm981a <linhcm981a@gmail.com>
2023-05-08 19:34:42 +0700

    setup environment deploy google could

---
+++ setup.sh
@@ -0,0 +1,72 @@
+
+set -eEo pipefail
+
+if [ ! -z "$CLOUD_SHELL" ]; then
+  printf "Checking for required npm version...\n"
+  npm install -g npm > /dev/null 2>&1
+  printf "Completed.\n\n"
+
+  printf "Setting up NVM...\n"
+  export NVM_DIR="/usr/local/nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+  printf "Completed.\n\n"
+  
+  printf "Updating nodeJS version...\n"
+  nvm install --lts
+  printf "Completed.\n\n"
+fi
+
+printf "Installing /microservice/back-end/gateway dependencies...\n"
+cd ./microservice/back-end/gateway
+npm install
+printf "Completed.\n\n"
+cd ..
+
+printf "Installing orderService dependencies...\n"
+cd ./orderService
+npm install
+printf "Completed.\n\n"
+cd ..
+
+printf "Installing productServices dependencies...\n"
+cd ./productServices
+npm install
+printf "Completed.\n\n"
+cd ..
+
+printf "Installing ratingService app dependencies...\n"
+cd ./ratingService
+npm install
+printf "Completed.\n\n"
+cd ..
+
+printf "Installing userService app dependencies...\n"
+cd ./userService
+npm install
+printf "Completed.\n\n"
+cd ..
+cd ..
+cd ..
+printf "Installing React app dependencies...\n"
+cd ./PhoneStore
+npm install
+printf "Completed.\n\n"
+
+printf "Building React app and placing into sub projects...\n"
+npm run build
+printf "Completed.\n\n"
+
+printf "Setup completed successfully!\n"
+
+if [ ! -z "$CLOUD_SHELL" ]; then
+  printf "\n"
+  printf "###############################################################################\n"
+  printf "#                                   NOTICE                                    #\n"
+  printf "#                                                                             #\n"
+  printf "# Make sure you have a compatible nodeJS version with the following command:  #\n"
+  printf "#                                                                             #\n"
+  printf "# nvm install --lts                                                           #\n"
+  printf "#                                                                             #\n"
+  printf "###############################################################################\n"
+fi
