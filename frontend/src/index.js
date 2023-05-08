c405e2205d3edda472ade1c0285e2395cf8cad87
linhcm981a <linhcm981a@gmail.com>
2023-05-08 21:06:03 +0700

    add ./ src

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import GlobalStyles from '~/components/GlobalStyles';
+import GlobalStyles from './GlobalStyles';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import store, { persistor } from '~/redux/store';
