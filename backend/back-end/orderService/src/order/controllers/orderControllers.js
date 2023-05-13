61bdb8afa7977fa1035cfc58d68eaae31ec4dbc3
linhcm981a <linhcm981a@gmail.com>
2023-05-13 20:17:57 +0700

    update call create order and rating aws

--- backend/back-end/orderService/src/order/controllers/orderControllers.js
+++ backend/back-end/orderService/src/order/controllers/orderControllers.js
@@ -5,16 +5,17 @@ const orderController = {
   createOrder: async (req, res) => {
     try {
       const { user, products, name, phone, address } = req.body;
-  
+
       if (!products) {
         return res.status(400).json({ message: "Products are required" });
       }
 
-      let userData ={};
+      let userData = {};
 
-      if(user){
-        const userResponse = await axios.get(`${process.env.USER_SERVICE_URL}/${user}`);
-  
+      if (user) {
+        const userResponse = await axios.get(
+          console.log("errro >>>>>", userResponse)
+            `${process.env.USER_SERVICE_URL}/${user}`);
         if (userResponse.status !== 200) {
           return res.status(userResponse.status).json({ message: "Failed to get user data", error: userResponse.data });
         }
@@ -24,23 +25,24 @@ const orderController = {
 
       const productsData = []
       for (const product of products) {
-        const productResponse = await axios.get(`${process.env.PRODUCT_SERVICE_URL}/${product.id}`);
+        const productResponse = await axios.get(console.log("errro >>>>>", productResponse)
+          `${process.env.PRODUCT_SERVICE_URL}/${product.id}`);
         if (productResponse.status !== 200) {
           return res.status(productResponse.status).json({ message: "Failed to get product data", error: productResponse.data });
         }
-        productResponse.data !==null && product.quantity <= productResponse.data.quantity && productsData.push({...productResponse.data, quantityInOrder: product.quantity})
+        productResponse.data !== null && product.quantity <= productResponse.data.quantity && productsData.push({ ...productResponse.data, quantityInOrder: product.quantity })
       }
 
       const order = new Order({
         userId: userData?._id,
-        products: productsData.map((productData)=>({
+        products: productsData.map((productData) => ({
           id: productData._id,
           name: productData.nameProduct,
           quantity: productData.quantityInOrder,
           price: (productData.price - productData.discount) || productData.price,
           image: productData.image,
         })),
-        totalPrice: productsData.reduce((acc, current) => acc + ((current.price - current.discount) || current.price)*current.quantityInOrder,0),
+        totalPrice: productsData.reduce((acc, current) => acc + ((current.price - current.discount) || current.price) * current.quantityInOrder, 0),
         name: name,
         phone: phone,
         address: address,
@@ -49,14 +51,14 @@ const orderController = {
       console.log("order", order);
 
       order.save()
-  
-      return res.status(200).json({ message: "Create new orders", order});
+
+      return res.status(200).json({ message: "Create new orders", order });
     } catch (error) {
       console.error(error);
       return res.status(500).json({ message: "Failed to create orders", error });
     }
   },
-  
+
   getAllOrders: async (req, res) => {
     try {
       const orders = await Order.find();
@@ -70,7 +72,7 @@ const orderController = {
   },
   getOrderUser: async (req, res) => {
     try {
-      const orders = await Order.find({userId: req.params.id});
+      const orders = await Order.find({ userId: req.params.id });
       if (!orders) {
         return res.status(404).json({ message: "Get order not found" });
       }
@@ -88,25 +90,25 @@ const orderController = {
         if (productResponse.status !== 200) {
           return res.status(productResponse.status).json({ message: "Failed to get product data", error: productResponse.data });
         }
-        productResponse.data !==null && product.quantity <= productResponse.data.quantity && productsData.push({...productResponse.data, quantityInOrder: parseInt(product.quantity)})
+        productResponse.data !== null && product.quantity <= productResponse.data.quantity && productsData.push({ ...productResponse.data, quantityInOrder: parseInt(product.quantity) })
       }
 
       console.log(productsData)
 
-      if(status === 'ĐÃ HỦY' || status === 'TRẢ HÀNG/HOÀN TIỀN'){
+      if (status === 'ĐÃ HỦY' || status === 'TRẢ HÀNG/HOÀN TIỀN') {
         for (const product of productsData) {
-          await axios.put(`${process.env.PRODUCT_SERVICE_URL}/${product._id}`, {quantity: product.quantity + product.quantityInOrder});
+          await axios.put(`${process.env.PRODUCT_SERVICE_URL}/${product._id}`, { quantity: product.quantity + product.quantityInOrder });
         }
-      }else if(status === 'ĐÃ XÁC NHẬN'){
+      } else if (status === 'ĐÃ XÁC NHẬN') {
         for (const product of productsData) {
-          await axios.put(`${process.env.PRODUCT_SERVICE_URL}/${product._id}`, {quantity: product.quantity - product.quantityInOrder});
+          await axios.put(`${process.env.PRODUCT_SERVICE_URL}/${product._id}`, { quantity: product.quantity - product.quantityInOrder });
         }
       }
 
       const order = await Order.findByIdAndUpdate(req.params.id, req.body, {
         new: true,
       });
-      return res.status(200).json({message: "successfully", order});
+      return res.status(200).json({ message: "successfully", order });
     } catch (error) {
       res.status(500).json({ message: "Order not found", error });
     }
